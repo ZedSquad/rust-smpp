d106523c8e2eda693d0f2833275cc971b912b9be
Andy Balaam <andybalaam@artificialworlds.net>
2021-01-05 16:59:22 +0000

    Use env_logger's default log format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use futures::prelude::*;
 use log::*;
-use std::io::Write;
 use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
@@ -10,23 +9,13 @@ type Result<T> =
     std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn main() {
-    init_logger();
+    env_logger::init();
 
     let mut rt = tokio::runtime::Runtime::new().unwrap();
     match rt.block_on(app()) {
         Ok(_) => info!("Done"),
         Err(e) => error!("Error launching: {}", e),
     };
-}
-
-fn init_logger() {
-    let start = std::time::Instant::now();
-    env_logger::Builder::from_default_env()
-        .format(move |buf, rec| {
-            let t = start.elapsed().as_secs_f32();
-            writeln!(buf, "{:08.03} {:7} {}", t, rec.level(), rec.args())
-        })
-        .init();
 }
 
 async fn app() -> Result<()> {
